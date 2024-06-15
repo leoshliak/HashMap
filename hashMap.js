f6bed6ddf0ed140968bebbd07192d8d4686d1f72
leoshliak <139138564+leoshliak@users.noreply.github.com>
2024-06-15 12:20:43 +0300

    has & remove & length

--- hashMap.js
+++ hashMap.js
@@ -1,50 +1,81 @@
 class HashMap {
-    #bucket;
-    #length;
-    #bucketSize;
-    
-    constructor(bucketSize = 16) {
-      this.#bucket = Array.from({ length: bucketSize }, () => []);
-      this.#length = 0;
-      this.#bucketSize = bucketSize;
+  #bucket;
+  #length;
+  #bucketSize;
+
+  constructor(bucketSize = 16) {
+    this.#bucket = Array.from({ length: bucketSize }, () => []);
+    this.#length = 0;
+    this.#bucketSize = bucketSize;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode %= this.#bucket.length;
     }
-   
-    hash(key) {
-        let hashCode = 0;
-           
-        const primeNumber = 31;
-        for (let i = 0; i < key.length; i++) {
-          hashCode = primeNumber * hashCode + key.charCodeAt(i);
-          hashCode %= this.#bucket.length;
-        }
-     
-        return hashCode;
-      } 
-      
-set(key, value) {
-       const index = this.hash(key);
-       const bucket = this.#bucket[index];
+    return hashCode;
+  }
 
-       for(let i = 0; i < bucket.length; i++) {
-           if(bucket[i].key === key) {
-            bucket[i].value = value;
-            return;
-           }
+  set(key, value) {
+    const index = this.hash(key);
+    const bucket = this.#bucket[index];
 
-           bucket.push({key, value});
-           this.#length++;
-       }
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i].key === key) {
+        bucket[i].value = value;
+        return;
       }
+    }
 
-      get(key) {
-        const index = this.hash(key);
-        const bucket = this.#bucket[index];
+    bucket.push({ key, value });
+    this.#length++;
+  }
 
-        for(let i = 0; i < bucket.length; i++) {
-          if(bucket[i].key === key) {
-           return bucket[i].value ;
-          }
+  get(key) {
+    const index = this.hash(key);
+    const bucket = this.#bucket[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i].key === key) {
+        return bucket[i].value;
       }
-      return null;
+    }
+    return null;
+  }
+
+  has(key) {
+    const index = this.hash(key);
+    const bucket = this.#bucket[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i].key === key) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    const bucket = this.#bucket[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i].key === key) {
+        bucket.splice(i, 1);
+        this.#length--;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  length() {
+    return this.#length;
+  }
 }
-}
+
+
+
